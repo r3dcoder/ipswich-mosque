e43f86d3788baa94cce9026ddc7b8b9f497b2fb0
r3dcoder <sdmahfuz@gmail.com>
2025-08-30 10:09:07 +0100

    env reset for db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,16 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-        View::composer('*', function ($view) {
-            $menus = Menu::whereNull('parent_id')->orderBy('order')->get();
-            $contacts = Contact::pluck('value', 'type')->toArray();
-
-            $view->with('headerData', [
-                'logo' => '/images/logo.png', // You can also add logo to DB or config
-                'menu' => $menus,
-                'contact' => $contacts,
-            ]);
-        });
+         
     }
 }
