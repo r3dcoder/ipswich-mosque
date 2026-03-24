012965adf530057af754e579379b37df8253fad3
r3dcoder <sdmahfuz@gmail.com>
2026-03-24 14:00:44 +0000

    change laravel version

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -13,6 +13,13 @@
       0 => 'Laravel\\Breeze\\BreezeServiceProvider',
     ),
   ),
+  'laravel/pail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Pail\\PailServiceProvider',
+    ),
+  ),
   'laravel/sail' => 
   array (
     'providers' => 
